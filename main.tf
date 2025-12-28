3709a4e0d0ad1453e20a2bd5846bbd670088eb65
rahulkumar0055 <rahulkumarnv332@gmail.com>
2025-12-28 08:46:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-    region = "us-east-1"  
+    region = "us-south-1"  
 }
 
 resource "aws_instance" "foo" {
-  ami           = "ami-05fa00d4c63e32376" # us-west-2
-  instance_type = "t2.micro"
+  ami           = "ami-02b8269d5e85954ef" # us-west-2
+  instance_type = "t3.micro"
   tags = {
       Name = "TF-Instance"
   }
